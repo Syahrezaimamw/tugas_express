998444cb74edf055a03b702ebd63a54a377943cb
Syahrezaimamw <syahrezaimamwahyudi976@gmail.com>
2024-04-30 20:50:44 +0700

    sss

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
- res.sendFile('./home.html' , {root:__dirname})
+  res.send('NAMA : syahreza imam w')
+//  res.sendFile('./home.html' , {root:__dirname})
 })
 
 app.listen(port, () => {
