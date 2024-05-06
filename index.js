99bd3e58ba5b9ce1d92e443a756a3e9f879f0c00
Syahrezaimamw <syahrezaimamwahyudi976@gmail.com>
2024-05-06 19:12:32 +0700

    ss

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('NAMA : SYAHREZA IMAM WAHYUDI </br> KELAS : XI RPL </br> HELLO WORLD')
 //  res.sendFile('./home.html' , {root:__dirname})
 })
+app.get('/json', (req, res) => {
+  res.json({username:'Syahrzeza Imam Wahyudi'})
+//  res.sendFile('./home.html' , {root:__dirname})
+})
+app.get('/cookies', (req, res) => {
+  res.json({username:'Syahrzeza Imam Wahyudi'})
+//  res.sendFile('./home.html' , {root:__dirname})
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
